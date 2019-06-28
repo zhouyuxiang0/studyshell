261b35c7509209fd5aae58cfd2f808c701a1b09b
1521350289@qq.com <1521350289@qq.com>
2019-06-28 23:29:24 +0800

    chore: string

--- test/string.sh
+++ test/string.sh
@@ -7,3 +7,11 @@ var3=${v1%ov*} # 从尾部匹配第一个ov并删除
 echo $var3
 var4=${v1%%ov*} # 从尾部匹配最后一个ov并删除
 echo $var4
+
+str1="zifuchuan"
+s1=${str1:2}
+echo $s1 # fuchuan
+s2=${str1:2:2}
+echo $s2 # fu
+s3=${str1: -5}
+echo $s3 # chuan
