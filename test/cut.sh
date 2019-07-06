dfedf3e0d956419235adb617232097377afd28d3
zhouyuxiang <1521350289@qq.com>
2019-07-06 21:22:09 +0800

    chore: get date

--- test/cut.sh
+++ test/cut.sh
@@ -12,3 +12,7 @@ done
   
 echo "This is `date +%Y` year"  # echo "This is $(date +%Y) year"
 echo "This is $(($(date +%Y)+1)) year" # $() 是命令替换  $(()) 是算数运算
+echo "This year have passed $(date +%j) days"
+echo "This year have passed $(($(date +%j)/7)) weeks" #今年过了过少星期  %j今年过的天数
+echo "There is $((365 - $(date +%j))) days before new year"
+echo "There is $(((365 - $(date +%j))/7)) weeks before new year"
